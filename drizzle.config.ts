083d425abd5ae6193857e9fbc055f935adabbe15
ToratYosef <yoseftorat@gmail.com>
2025-11-28 01:08:32 +0000

    s

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default {
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_zyuokg5L6ACf@ep-cool-violet-a4ea6weg-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require",
+    url: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_zyuokg5L6ACf@ep-cool-violet-a4ea6weg-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   },
 } satisfies Config;
